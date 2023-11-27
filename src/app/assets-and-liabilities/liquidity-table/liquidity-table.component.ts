0aac8e4556fb824ab9d3db3ec446e2c3f50e6013
Andreas Landerer <andreaslanderer@gmail.com>
2023-11-27 13:48:49 +0100

    Updated Assets & Liabilities - added security assets & liabilities

--- src/app/assets-and-liabilities/liquidity-table/liquidity-table.component.ts
+++ src/app/assets-and-liabilities/liquidity-table/liquidity-table.component.ts
@@ -1,14 +1,14 @@
 import {Component, Input} from '@angular/core';
-import {LiquidityEntry} from "../assets-and-liabilities.result";
+import {AssetEntry} from "../assets-and-liabilities.result";
 
 @Component({
-  selector: 'app-liquidity-table',
+  selector: 'app-asset-table',
   templateUrl: './liquidity-table.component.html',
   styleUrls: ['./liquidity-table.component.css']
 })
 export class LiquidityTableComponent {
 
-  @Input() entries: LiquidityEntry[]
+  @Input() entries: AssetEntry[]
   @Input() background: string
 
   opened = false
